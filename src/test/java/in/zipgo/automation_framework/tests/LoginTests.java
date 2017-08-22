4607bdc952ac6ae7c330b53bf15c6337c475fe40
Rajesh Iyer <rajesh23427@gmail.com>
2017-08-22 22:34:30 +0530

    Pushing all the code that I have done so far

--- src/test/java/in/zipgo/automation_framework/tests/LoginTests.java
+++ src/test/java/in/zipgo/automation_framework/tests/LoginTests.java
@@ -3,19 +3,23 @@ package in.zipgo.automation_framework.tests;
 import in.zipgo.automation_framework.pages.web.DashboardPage;
 import in.zipgo.automation_framework.pages.web.LoginPage;
 import in.zipgo.automation_framework.pages.web.SignInPage;
+import in.zipgo.automation_framework.utils.Excel;
+import in.zipgo.automation_framework.utils.ExcelContext;
 import in.zipgo.automation_framework.workflow.ZipGoWorkFlowImplementations;
 import in.zipgo.automation_framework.workflow.ZipGoWorkflows;
+import javax.swing.JOptionPane;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class LoginTests extends BaseTests {
 
     @Test(dataProvider = "testData")
-    public void loginTests(String username, String password, boolean isLoginSuccessful) {
+    public void loginTests(String username, String password, String isLoggedIn) {
         ZipGoWorkflows zgw = new ZipGoWorkFlowImplementations();
         SignInPage signInPage = zgw.openSignInPage();
         LoginPage loginPage = zgw.navigateToLoginPage(signInPage);
         DashboardPage dashboardPage = null;
+        boolean isLoginSuccessful = Boolean.valueOf(isLoggedIn);
         if (isLoginSuccessful) {
             dashboardPage = (DashboardPage) zgw.loginToTheSystem(loginPage, username, password, DashboardPage.class);
         } else {
@@ -26,6 +30,11 @@ public class LoginTests extends BaseTests {
 
     @DataProvider(name = "testData", parallel = true)
     public static Object[][] getTestData() {
-        return new Object[][]{{"rajesh23427@gmail.com", "1234", false}, {"rajesh23427@gmail.com", "12345", true}};
+        ExcelContext context = new ExcelContext();
+        context.setExcelFile("testData/Login.xlsx");
+        context.setSheetName("login");
+        Excel excelObj = new Excel(context);
+        String[][] testData = excelObj.getData("username", "password", "isLoggedIn");
+        return testData;
     }
 }
